cfccb400ae64afe503388b54b8d6f9dc794f46df
zn-rabby <rabby.webdeveloper@gmail.com>
2025-03-21 23:02:25 +0600

    all isses solve

--- src/service/AuthService/index.ts
+++ src/service/AuthService/index.ts
@@ -4,26 +4,7 @@ import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
 
-export const registerUser = async (userData: FieldValues) => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/register`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
-    const result = await res.json();
 
-    if (result.success) {
-      (await cookies()).set("accessToken", result.data.token);
-    }
-
-    return result;
-  } catch (error: any) {
-    return Error(error);
-  }
-};
 
 export const loginUser = async (userData: FieldValues) => {
   try {
